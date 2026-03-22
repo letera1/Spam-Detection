c484a5fa9af6eb4504dfd681416a0336e75c2921
Letera Tujo <letera9tujo@gmail.com>
2026-03-22 14:25:14 +0300

    complet

--- backend/inference/__init__.py
+++ backend/inference/__init__.py
@@ -1,10 +1,8 @@
 """Inference package for spam detection predictions."""
 
 from .predictor import SpamPredictor, PredictionResult
-from .batch_predictor import BatchPredictor
 
 __all__ = [
     "SpamPredictor",
     "PredictionResult",
-    "BatchPredictor",
 ]
